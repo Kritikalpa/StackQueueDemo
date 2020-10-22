0b9adc23becf0073628901898b344e72e49d4a54
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-22 13:27:45 +0530

    first commit

---
+++ StackDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack Demo");
+        }
+    }
+}
