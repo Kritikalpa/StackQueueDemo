3976f1a1ef9168fe8e411d7228fea77a8c690f5e
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-22 13:32:49 +0530

    [Kriti] Add. Push element is stack

---
+++ StackDemo/LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackDemo
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackDemo/Program.cs
+++ StackDemo/Program.cs
@@ -7,6 +7,11 @@ namespace StackDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack Demo");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
